89f729abb44943f7d84b167680d3960fb9ccc7fc
PrakharSuraj <prakharsurajwipro@gmail.com>
2025-02-01 11:22:01 +0530

    Update HelloWorld2.java

--- wipro_javaee_project/src/com/wipro/javaee/HelloWorld2.java
+++ wipro_javaee_project/src/com/wipro/javaee/HelloWorld2.java
@@ -20,7 +20,7 @@ public class HelloWorld2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is My Second Java Project");	
+		System.out.println("This Is My Second Java Project");	
 
 	}
 
