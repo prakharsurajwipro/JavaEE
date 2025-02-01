e9e4fd7396037fd151d18f064bee19492fc101b6
PrakharSuraj <prakharsurajwipro@gmail.com>
2025-02-01 10:59:56 +0530

    commit from main

---
+++ wipro_javaee_project/src/com/wipro/javaee/HelloWorld2.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.wipro.javaee;
+
+/**
+ * @author prakharsurajwipro
+ */
+public class HelloWorld2 {
+
+	/**
+	 * 
+	 */
+	public HelloWorld2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
